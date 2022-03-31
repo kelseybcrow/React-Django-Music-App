4237eb238b141d7b608428cc8c4c9f861d624d84
Kelsey Crow <kelseybcrow@gmail.com>
2022-03-31 17:03:49 -0600

    rendering the create-room-page with react

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -32,10 +32,11 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
-            return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class RoomJoinPage extends Component {
+export default class CreateRoomPage extends Component {
     constructor(props) {
         super(props)
     }
