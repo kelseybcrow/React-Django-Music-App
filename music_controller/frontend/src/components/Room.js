cb85900afbcb8470ee5e373687b1ad4c2da6c48c
Kelsey Crow <kelseybcrow@gmail.com>
2022-04-06 18:24:18 -0600

    call API endpoints from React

---
+++ music_controller/frontend/src/components/Room.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Room extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Votes: {this.state.votesToSkip}</p>
+                <p>Guest Can Pause: {this.state.guestCanPause}</p>
+                <p>Host: {this.state.isHost}</p>
+            </div>
+        )
+    }
+}
