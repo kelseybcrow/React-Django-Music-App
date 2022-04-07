7b8a2c70f8e660c1b6ef0ff4df20833b706c0f30
Kelsey Crow <kelseybcrow@gmail.com>
2022-04-07 09:52:08 -0600

    build out room-join page

--- music_controller_tutorial_7/frontend/src/components/RoomJoinPage.js
+++ music_controller_tutorial_7/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,45 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import { TextField, Button, Grid, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 export default class RoomJoinPage extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = {
+      roomCode: '',
+      error: '',
+    }
   }
 
   render() {
-    return <p>This is the room join page</p>;
+    return (
+      <Grid container spacing={1} alignItems='center'>
+        <Grid item xs={12}>
+          <Typography variant='h4' component='h4'>
+            Join a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            error={this.state.error}
+            label='Code'
+            placeholder='Enter a Room Code'
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant='outlined'
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant='contained' color='primary' onClick>
+            Enter Room
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant='contained' color='secondary' to='/' component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    )
   }
 }
